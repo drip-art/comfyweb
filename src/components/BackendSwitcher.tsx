91a60968669e960b86a198930601f37c8643d2a6
snomiao <snomiao@gmail.com>
2024-05-24 07:51:40 +0000

    fix(components): BackendSwitcher
    BackendSwitcher

--- src/components/BackendSwitcher.tsx
+++ src/components/BackendSwitcher.tsx
@@ -1,17 +1,21 @@
-import { useLocalStorage } from "usehooks-ts"
-import { getBackendUrl } from "../config"
+import { useLocalStorage } from 'usehooks-ts'
+import { getBackendUrl } from '../config'
 
 export default function BackendSwitcher() {
-    const [comfywebConfig, setComfywebConfig] = useLocalStorage('comfyweb-config', globalThis.comfywebConfig)
+  const [comfywebConfig, setComfywebConfig] = useLocalStorage('comfyweb-config', globalThis.comfywebConfig)
 
-    return <form onSubmit={(event) => {
+  return (
+    <form
+      onSubmit={(event) => {
         setComfywebConfig(event.currentTarget.form.backend_url.value)
         alert('New Backend URL:' + getBackendUrl(''))
-    }}>
-        <label>
-            Backend URL:
-            <input name='backend_url' defaultValue={comfywebConfig} />
-        </label>
-        <button >Submit</button>
+      }}
+    >
+      <label>
+        Backend URL:
+        <input name="backend_url" defaultValue={comfywebConfig} />
+      </label>
+      <button>Submit</button>
     </form>
+  )
 }
