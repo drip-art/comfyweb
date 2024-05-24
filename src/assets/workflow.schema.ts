c936a30dd62ed40e78eeffe6ef2b93596078342b
snomiao <snomiao@gmail.com>
2024-05-24 06:59:17 +0000

    fix(assets): update widget schema
    update widget schema

---
+++ src/assets/workflow.schema.ts
@@ -0,0 +1,21 @@
+// import { WorkflowData } from "../workflowSchema";
+import WF20240523 from './workflow-2024-05-23.json'
+import WFDefault from './defaultWorkflow.json'
+
+// export type WorkflowNode = {
+//   // TODO
+// }
+// export type WorkflowData = {
+//   nodes: WorkflowNode[]
+//   last_node_id?: number
+//   last_link_id?: number
+//   links?: (string | num112ber)[][]
+//   groups?: any[]
+//   config?: {}
+//   extra?: {
+//     groupNodes: { testerror: {} }
+//   }
+//   version?: number
+// }
+export type WorkflowSchema = typeof WF20240523 & typeof WFDefault
+ 
