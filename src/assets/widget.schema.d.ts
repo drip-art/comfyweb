04496d552deca5e97310d906a532336a0a932ef8
snomiao <snomiao@gmail.com>
2024-05-24 09:36:13 +0000

    fix(assets): widgets update
    widgets update

--- src/assets/widget.schema.d.ts
+++ src/assets/widget.schema.d.ts
@@ -1,3 +1,3 @@
-import Widgets20240523 from './widgets-2024-05-23.json';
-export type WidgetsSchema = typeof Widgets20240523;
-export type Widget = WidgetsSchema[keyof WidgetsSchema];
+import Widgets20240523 from './widgets-2024-05-23.json'
+export type WidgetsSchema = typeof Widgets20240523
+export type Widget = WidgetsSchema[keyof WidgetsSchema]
--- src/assets/workflow.schema.d.ts
+++ src/assets/workflow.schema.d.ts
@@ -1,4 +1,4 @@
-import WF20240523 from './workflow-2024-05-23.json';
-import WFDefault from './defaultWorkflow.json';
-export type WorkflowSchema = typeof WF20240523 & typeof WFDefault;
-export { WF20240523, WFDefault };
+import WF20240523 from './workflow-2024-05-23.json'
+import WFDefault from './defaultWorkflow.json'
+export type WorkflowSchema = typeof WF20240523 & typeof WFDefault
+export { WF20240523, WFDefault }
