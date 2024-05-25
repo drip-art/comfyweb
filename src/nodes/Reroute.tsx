00fe185c8c8dce753072c56ba2c1f018fa71b580
snomiao <snomiao@gmail.com>
2024-05-25 04:36:16 +0000

    fix(nodes): todo reroute
    todo reroute

---
+++ src/nodes/Reroute.tsx
@@ -0,0 +1,29 @@
+// import { memo } from 'react'
+// import { useAppStore } from '../store'
+// import { Widget, WidgetLegacy } from '../types'
+// // useAppStore.setState((s) => (s.widgets.Reroute = RerouteNode))
+// const widget = {
+//   name: 'Reroute',
+//   category: 'Group',
+//   input: { required: { '': ['*'] } },
+
+//   output: ['*'],
+// } satisfies WidgetLegacy
+
+// function RerouteNode({ node }: { node: Widget }) {
+//   return (
+//     <>
+//       <div style={{ padding: 10, display: 'flex' }}>
+//         <div style={{ marginRight: 4 }}>{data.level}.</div>
+//         <div>{data.label}</div>
+//       </div>
+//       {data.arrowStyle && (
+//         <div className="arrow" style={data.arrowStyle}>
+//           ⤹
+//         </div>
+//       )}
+//     </>
+//   )
+// }
+
+// export default memo(RerouteNode)
